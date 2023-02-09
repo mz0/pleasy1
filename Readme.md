967171874ff29e3587a73919e579b6d5a43c76d6
Mark Zhitomirski <mz@exactpro.com>
2023-02-09 20:13:53 +0400

    do not load (instantiate) "plugins" from stop-list (Java 9+)
    using java.util.ServiceLoader.Provider

--- Readme.md
+++ Readme.md
@@ -1,29 +1,27 @@
 run with `./gradlew app:run`
 
-needs Java 11+, Java 8 cannot build SWT here
+needs Java 11+ (ServiceLoader.Provider is a Java 9+ Interface)
 
 console output (after 2 clicks):
 ```
 > Task :app:run (Java 17)
+will not load: bb.spi.pf.ProtocolF
 bb.spi.pa.ProtocolA instantiated
+bb.spi.pb.ProtocolB instantiated
+collecting supported protocol IDs
 adding ID 'agx' for bb.spi.pa.ProtocolA
 adding ID 'abc' for bb.spi.pa.ProtocolA
 adding ID 'apx' for bb.spi.pa.ProtocolA
-bb.spi.pb.ProtocolB instantiated
 adding ID 'dkw' for bb.spi.pb.ProtocolB
 WARNING: ID 'abc' for bb.spi.pb.ProtocolB conflicts with bb.spi.pa.ProtocolA
 adding ID 'mbb' for bb.spi.pb.ProtocolB
 adding ID 'bmw' for bb.spi.pb.ProtocolB
-bb.spi.pf.ProtocolF instantiated
-WARNING: ID 'dkw' for bb.spi.pf.ProtocolF conflicts with bb.spi.pb.ProtocolB
-WARNING: ID 'abc' for bb.spi.pf.ProtocolF conflicts with bb.spi.pa.ProtocolA
-WARNING: ID 'mbb' for bb.spi.pf.ProtocolF conflicts with bb.spi.pb.ProtocolB
-WARNING: ID 'bmw' for bb.spi.pf.ProtocolF conflicts with bb.spi.pb.ProtocolB
 
-2 plugins loaded OK (5 warning[s]) in 20 ms
+2 plugins loaded OK (1 warning[s]) in 25 ms
+
 
 Compatible IDs: dkw, agx, abc, apx, mbb, bmw
 Compatible IDs: dkw, agx, abc, apx, mbb, bmw
 ```
-Note 3 instantiations (ProtocolA, ProtocolB, ProtocolF), but only 2 of them are used ("loaded")  
-due to conflicts (all 4 IDs of ProtocolF conflict with ProtocolB)
+Note 2 instantiations (ProtocolA, ProtocolB), both of them are used ("loaded" indeed)  
+ProtocolF not loaded due to (hardcoded) configuration rule
--- core/src/main/java/bb/core/PluginManager.java
+++ core/src/main/java/bb/core/PluginManager.java
@@ -2,23 +2,24 @@ package bb.core;
 
 import bb.api.ProtocolPlugin;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider; // since Java 9
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class PluginManager {
-    private static ServiceLoader<ProtocolPlugin> loader = ServiceLoader.load(ProtocolPlugin.class);
-    private Map<String, ProtocolPlugin> protocolHandlers = new HashMap<>();
+    private final Map<String, ProtocolPlugin> protocolHandlers = new HashMap<>();
 
     public PluginManager() {
+        List<String> stopList = List.of("bb.spi.pf.ProtocolF");
         long start = System.currentTimeMillis();
+        List<ProtocolPlugin> loadPlugins = enabledPlugins(stopList);
         int warnings = 0;
-        for (ProtocolPlugin pp : loader) {
+        System.out.println("collecting supported protocol IDs");
+        for (ProtocolPlugin pp : loadPlugins) {
             for (String id : pp.getCompatibleIDs()) {
                 if (protocolHandlers.get(id) == null) {
                     System.out.printf("adding ID '%s' for %s%n", id, pp.getClass().getName());
@@ -32,10 +33,19 @@ public class PluginManager {
         }
         long finish = System.currentTimeMillis();
         System.out.printf("%n%d plugins loaded OK (%d warning[s]) in %d ms%n%n%n",
-                protocolHandlers.values().stream().distinct().count(), warnings, finish - start);
+                loadPlugins.size(), warnings, finish - start);
     }
 
     public Set<String> getCompatibleIDs() {
         return protocolHandlers.keySet();
+    }
+
+    private static List<ProtocolPlugin> enabledPlugins(List<String> stopList) {
+        System.out.println("will not load: " + String.join(", ", stopList));
+        return ServiceLoader.load(ProtocolPlugin.class)
+                .stream()
+                .filter((Provider<ProtocolPlugin> p) -> !stopList.contains(p.type().getName()))
+                .map(Provider::get)
+                .collect(Collectors.toList());
     }
 }
