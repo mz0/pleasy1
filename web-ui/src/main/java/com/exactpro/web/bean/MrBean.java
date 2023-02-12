730a562ae498eab026f526c38965d16f43f54954
Mark Zhitomirski <mz@exactpro.com>
2023-02-12 20:30:34 +0400

    web-ui WIP3: add CDI 1.2 and explain it, etc.

--- web-ui/src/main/java/com/exactpro/web/bean/MrBean.java
+++ web-ui/src/main/java/com/exactpro/web/bean/MrBean.java
@@ -4,13 +4,35 @@ import bb.core.PluginManager;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("unused") // index.xhtml
 @ManagedBean(name = "mrBean")
 @ViewScoped
 public class MrBean implements Serializable {
-    private Map<String, String> proto2plugin = PluginManager.getInstance().getProtocolHandlerNames();
+    private final Map<String, String> proto2plugin = PluginManager.getInstance().getProtocolHandlerNames();
 
-    public Map<String, String> getMap() { return proto2plugin; }
-    public void setMap(Map<String, String> proto2className) { this.proto2plugin = proto2className; }
+    public List<Mapping> getList() {
+        return proto2plugin.entrySet().stream().map(Mapping::new).collect(Collectors.toList());
+    }
+
+    public static class Mapping {
+        String key;
+        String value;
+
+        public String getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public Mapping(Map.Entry kv) {
+            key = (String) kv.getKey();
+            value = (String) kv.getValue();
+        }
+    }
 }
