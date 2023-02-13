123f74a8eb8dedd00bd1f889225aac16d3fd4f90
Mark Zhitomirski <mz@exactpro.com>
2023-02-13 16:04:09 +0400

    restore SWT version, upgrade Gradle 7.6 -> 8.0

--- Readme.md
+++ Readme.md
@@ -1,11 +1,8 @@
 ## This is a techology playground and a WIP
 
-see {app}(app/Readme.md)  
-run it with `./gradlew app:run` (from this dir)  
- OR  
-`./gradlew web-ui:appRunWar` (and press any key to stop Jetty)
-
-See also [web-ui/Readme](web-ui/Readme.md) on the JSF 2.3 version of this demo
+run it:
+- `./gradlew app:run` [SWT version](app/Readme.md)
+ OR
+- `./gradlew web-ui:appRunWar` (and press Enter to stop Jetty) [JSF 2.3 version](web-ui/Readme.md)
 
 needs Java 11+ (ServiceLoader.Provider is a Java 9+ Interface)
-
--- app/src/main/java/bb/app/Main.java
+++ app/src/main/java/bb/app/Main.java
@@ -1,37 +1,46 @@
+/* a starter using https://www.vogella.com/tutorials/SWT/article.html */
 package bb.app;
 
 import bb.core.PluginManager;
-
-import javax.swing.*;
-import java.awt.FlowLayout;
-import java.lang.reflect.InvocationTargetException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class Main {
-    private static void createAndShowGUI() {
-        JFrame frm0 = new JFrame("HelloWorldSwing");
-        frm0.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JDialog dlg1 = new JDialog(frm0, "the owls are not what they seem", true);
-        dlg1.setLayout(new FlowLayout());
-        JButton btnOK = new JButton("OK");
-        btnOK.addActionListener(e -> onClick());
-        dlg1.add(new JLabel("click there >"));
-        dlg1.add(btnOK);
-        dlg1.setSize(310, 155);
-        dlg1.setVisible(true);
-     // frm0.pack(); // TODO check if needed
-     // frm0.setVisible(true);
+    public static void main(String[] args) {
+        PluginManager.init();
+        String btnText = "Click here";
+        Display display = new Display();
+        try {
+          final Shell shell = new Shell(display);
+          shell.setText("SWT app0");
+          shell.setLayout(new FillLayout());
+          Button button = new Button(shell, SWT.PUSH);
+          button.setText(btnText);
+          button.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                onClick();
+            }
+          });
+          shell.open();
+          while (!shell.isDisposed())
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        } finally {
+          display.dispose();
+        }
     }
 
     private static void onClick() {
-        System.out.println("Compatible IDs: " + getIDs(PluginManager.getInstance()));
-        PluginManager.getInstance().getProtocolHandlerNames().entrySet()
-            .forEach(kv -> System.out.printf("ProtocolId %s - Handler %s%n", kv.getKey(), kv.getValue()));
-    }
-
-    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
-        PluginManager.init();
-        javax.swing.SwingUtilities.invokeAndWait(Main::createAndShowGUI);
-        System.exit(0);
+        PluginManager pm = PluginManager.getInstance();
+        System.out.println("Compatible IDs: " + getIDs(pm));
+        pm.getProtocolHandlerNames().forEach((key, val) -> System.out.printf("ProtocolId %s - Handler %s%n", key, val));
     }
 
     private static String getIDs(PluginManager pm) {
